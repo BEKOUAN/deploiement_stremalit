7564e14282672837c9878c3b0606ef223d160740
BEKOUAN MARTIAL <bekouan@gmail.com>
2025-09-03 19:36:06 +0000

    'first_commit'

---
+++ projetBekouan_streamlit.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import pickle
+
+# Load the model
+model = pickle.load(open('projetBekouan.pkl', 'rb'))
+st.title('PREDICTION DU SENTIENT EN FONCTION D' 'UN TWEET')
+
+st.header('Veuillez renseigner les données en entrée')
+
+#Données entrée par l'utilisateur
+pannee = st.slider('Année du tweet', 2013,2023)
+pmois = st.slider('mois du tweet', 1,12)
+pjour = st.slider('jour du tweet', 1,31)
+psite = st.radio('plateforme :',('Facebook', 'Twitter', 'Instagram'))
+pperiode = st.radio('période :', ('matin','midi', 'soir'))
+
+# Predict house price
+site = 0
+periode = 0
+if st.button('Prédire le sentiment'):
+    if psite == 'Facebook':
+        site = 1
+    elif psite == 'Instagram':
+        site = 2
+    else :
+        site = 0
+
+    if pperiode == 'midi':
+     periode = 1
+    elif pperiode == 'soir':
+        periode = 2
+    else:
+        periode = 2
+
+    prediction = model.predict([[pannee, pmois, pjour, periode,site]])
+    lib_pred = ''
+    if prediction == 0 :
+        lib_pred = 'positive'
+    elif lib_pred == 1:
+        lib_pred = 'Neutre'
+    else:
+        lib_pred = 'Négative'
+    #Affichage du sentiment
+    st.write(f'La prédiction du sentiment : {lib_pred}')
